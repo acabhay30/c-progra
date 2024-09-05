c181b9154b2bbe9f2247fedd2c90105952154737
acabhay30 <85096259+acabhay30@users.noreply.github.com>
2024-09-05 22:22:16 +0530

    done

--- chapter3/justtry.c
+++ chapter3/justtry.c
@@ -1,21 +1,46 @@
-#include<stdio.h>
+// #include<stdio.h>
+
+// int main()
+// {
+//     int a,b;
+//     printf("enter the number1\n");
+//     scanf("%d", &a);
+//     printf("enter the number2\n");
+//     scanf("%d", &b);
+
+//     if (a<b)
+//     {
+//         printf("the number2:%d\n", b);
+//     }
+//     else
+//     {
+//         printf("the number1:%d\n", a);
+//     }
+//     return 0;
+
+// }
+
+//
+
+#include <stdio.h>
 
 int main()
 {
-    int a,b;
+    int a;
     printf("enter the number1\n");
     scanf("%d", &a);
-    printf("enter the number2\n");
-    scanf("%d", &b);
-
-    if (a<b)
+    if (a % 5 == 0)
     {
-        printf("the number2:%d\n", b);
+        printf("this number is divisible %d to 5", a);
+    }
+    else if (a % 11 == 0)   
+    {
+        printf("this number is divisible %d to 11", a);
     }
     else
     {
-        printf("the number1:%d\n", a);
+        printf("The number %d is not divisible with 5 nor 11", a);
     }
     return 0;
+}  
 
-}
